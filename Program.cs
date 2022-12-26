4aced2303ee44f10957a5edf6cc9eec7f0169c67
Ira <airichka@mail.ru>
2022-12-26 21:12:45 +0400

    prog

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите через запятую элементы массива: ");
+var userInput = Console.ReadLine();
+
+if (!string.IsNullOrWhiteSpace(userInput))
+{
+	var words = ParseWords(userInput);
+	var wordsWhith3Chars = GetWordsWith3Chars(words);
+
+	foreach (var word in wordsWhith3Chars)
+	{
+		Console.WriteLine(word);
+	}
+}
+else
+{
+	Console.WriteLine("Вы ввели не верное значение");
+}
+
+IEnumerable<string> ParseWords(string userInput)
+{
+	return userInput.Replace(" ", string.Empty).Split(',');
+}
+
+IEnumerable<string> GetWordsWith3Chars(IEnumerable<string> strings)
+{
+	return strings.Where(x => x.Length <= 3);
+}
